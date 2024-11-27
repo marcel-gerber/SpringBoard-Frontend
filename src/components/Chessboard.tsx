c57a5f723d05b519cfb541f85bc4ea02da43388e
Marcel Gerber <marcelgerber9@gmail.com>
2024-11-27 18:26:19 +0100

    Made Squares clickable

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -1,38 +1,47 @@
+import {useState} from "react";
 import {Board} from "../chesslogic/Board.ts";
 import {Piece} from "../chesslogic/pieces/Piece.ts";
+import {Move} from "../chesslogic/Move.ts";
 
 export default function Chessboard() {
     const board: Board = new Board();
     board.setFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
 
+    const legalMoves: Array<Move> = board.getLegalMoves();
+    const [selectedSquare, setSelectedSquare] = useState<number | null>(null);
+
     function getSquareColor(row: number, col: number): string {
         return (row + col) % 2 === 0 ? "bg-gray-200" : "bg-gray-700";
     }
 
-    let index: number = 55;
-    let row: number = -1;
+    function handleClickSquare(index: number): void {
+        setSelectedSquare(index);
+
+        for(const move of legalMoves) {
+            if(move._from.getIndex() == index) console.log(move);
+        }
+    }
 
     return (
         <div className="grid grid-cols-8 w-[700px]">
-            {board.pieces.map(() => {
-                index++;
-                const col: number = index % 8;
+            {board.pieces.map((_, idx) => {
+                const row = Math.floor(idx / 8);
+                const col = idx % 8;
 
-                if(col == 0) {
-                    row++;
-
-                    if(row != 0) {
-                        index -= 16;
-                    }
-                }
+                // Calculate index in a LERF-Mapping
+                const index = 56 - row * 8 + col;
 
                 const piece: Piece = board.getPiece(index);
                 const pieceImage: string | null = piece.image();
 
+                const isSelected: boolean = selectedSquare === index;
+
                 return (
                     <div
-                        key={index}
-                        className={`${getSquareColor(row, col)} flex items-center justify-center aspect-square`}
+                        key={idx}
+                        className={`${getSquareColor(row, col)} ${isSelected ? "border-4 border-red-500" : ""}
+                        flex items-center justify-center aspect-square`}
+                        onClick={() => handleClickSquare(index)}
                     >
                         {pieceImage && <img src={pieceImage} alt={piece.character()} className="w-full h-full"/>}
                     </div>
