a54a9a2ca9ea46c8f6b5a51784fe36d4c8fdbb5e
Marcel Gerber <marcelgerber9@gmail.com>
2024-11-27 20:49:17 +0100

    Added attackedSquares

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -6,9 +6,10 @@ import {Move} from "../chesslogic/Move.ts";
 export default function Chessboard() {
     const board: Board = new Board();
     board.setFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
-
     const legalMoves: Array<Move> = board.getLegalMoves();
+
     const [selectedSquare, setSelectedSquare] = useState<number | null>(null);
+    const [attackedSquares, setAttackedSquares] = useState<Array<number>>([]);
 
     function getSquareColor(row: number, col: number): string {
         return (row + col) % 2 === 0 ? "bg-gray-200" : "bg-gray-700";
@@ -17,9 +18,10 @@ export default function Chessboard() {
     function handleClickSquare(index: number): void {
         setSelectedSquare(index);
 
-        for(const move of legalMoves) {
-            if(move._from.getIndex() == index) console.log(move);
-        }
+        setAttackedSquares(legalMoves
+            .filter(value => value._from.getIndex() == index)
+            .map(value => value._to.getIndex()));
+        console.log(attackedSquares);
     }
 
     return (
