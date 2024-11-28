1e98ee7b314e371d2804fab2d6ce42f3a6ff9fba
Marcel Gerber <marcelgerber9@gmail.com>
2024-11-28 23:46:18 +0100

    Added drag and drop of pieces

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -1,27 +1,58 @@
-import {useState} from "react";
+import {useEffect, useReducer, useState} from "react";
 import {Board} from "../chesslogic/Board.ts";
 import {Piece} from "../chesslogic/pieces/Piece.ts";
 import {Move} from "../chesslogic/Move.ts";
+import {NullPiece} from "../chesslogic/pieces/NullPiece.ts";
+
+function boardReducer(state: Board, action: { type: "move"; move: Move }): Board {
+    const newBoard = state.copy();
+    newBoard.makeMove(action.move);
+    return newBoard;
+}
 
 export default function Chessboard() {
-    const board: Board = new Board();
-    board.setFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
-    const legalMoves: Array<Move> = board.getLegalMoves();
+    const initialBoard = new Board();
+    initialBoard.setFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
 
-    const [selectedSquare, setSelectedSquare] = useState<number | null>(null);
+    const [board, dispatcher] = useReducer(boardReducer, initialBoard);
     const [attackedSquares, setAttackedSquares] = useState<Array<number>>([]);
+    const [draggedPiece, setDraggedPiece] = useState<number | null>(null);
+    const [legalMoves, setLegalMoves] = useState<Array<Move>>(board.getLegalMoves());
+
+    // Update legalMoves when board gets updated
+    useEffect(() => {
+        setAttackedSquares([]);
+        setLegalMoves(board.getLegalMoves());
+    }, [board]);
 
     function getSquareColor(row: number, col: number): string {
         return (row + col) % 2 === 0 ? "bg-gray-200" : "bg-gray-700";
     }
 
-    function handleClickSquare(index: number): void {
-        setSelectedSquare(index);
-
+    function updateAttackedSquares(index: number): void {
         setAttackedSquares(legalMoves
             .filter(value => value._from.getIndex() == index)
             .map(value => value._to.getIndex()));
-        console.log(attackedSquares);
+    }
+
+    function handleDragStart(index: number): void {
+        updateAttackedSquares(index);
+        setDraggedPiece(index);
+    }
+
+    function handleDragOver(event: React.DragEvent): void {
+        event.preventDefault();
+    }
+
+    function handleDrop(index: number): void {
+        if(draggedPiece === null) return;
+
+        const move: Move | undefined = legalMoves.find(
+            (move) => move._from.getIndex() === draggedPiece && move._to.getIndex() === index
+        );
+        if(!move) return;
+
+        dispatcher({ type: "move", move });
     }
 
     return (
@@ -36,16 +67,28 @@ export default function Chessboard() {
                 const piece: Piece = board.getPiece(index);
                 const pieceImage: string | null = piece.image();
 
-                const isSelected: boolean = selectedSquare === index;
+                const isSquareAttacked: boolean = attackedSquares.includes(index);
+                const isPieceAttacked: boolean = !(piece instanceof NullPiece) && isSquareAttacked;
 
                 return (
                     <div
                         key={idx}
-                        className={`${getSquareColor(row, col)} ${isSelected ? "border-4 border-red-500" : ""}
-                        flex items-center justify-center aspect-square`}
-                        onClick={() => handleClickSquare(index)}
+                        className={`${getSquareColor(row, col)} ${isPieceAttacked ? "border-4 border-red-500" : ""}
+                        flex items-center justify-center aspect-square relative`}
+                        onClick={() => updateAttackedSquares(index)}
+                        onDragOver={handleDragOver}
+                        onDrop={() => handleDrop(index)}
                     >
-                        {pieceImage && <img src={pieceImage} alt={piece.character()} className="w-full h-full"/>}
+                        {pieceImage && (
+                            <img
+                                src={pieceImage}
+                                alt={piece.character()}
+                                className="w-full h-full cursor-pointer"
+                                draggable
+                                onDragStart={() => handleDragStart(index)}
+                            />
+                        )}
+                        {isSquareAttacked && !isPieceAttacked && <div className="absolute w-7 h-7 bg-gray-400 rounded-full"></div>}
                     </div>
                 );
             })}
