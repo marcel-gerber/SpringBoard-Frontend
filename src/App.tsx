9f53165b8bfbb1c247ec57b5a941fd842608189a
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-04 20:24:07 +0100

    Added Router to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./styles/App.css";
 import Home from "./pages/Home.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Signup from "./pages/./Signup.tsx";
 
 export default function App() {
     return (
-        <>
-            <Home/>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
