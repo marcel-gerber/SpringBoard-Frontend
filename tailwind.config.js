fb4061c785119ae6596f1584f99888f1c2fb9d35
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-04 20:22:36 +0100

    Tried to create a fancy background

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'custom-radial': 'radial-gradient(circle at right top, #141414, rgb(22 163 74 / 0.2), rgb(2 132 199 / 0.2), #141414)',
       }
     },
   },
