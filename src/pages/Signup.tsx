b4171e152ed3572298c9da5a352040b38aa2eb0d
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-06 00:16:26 +0100

    Continued Signup-page

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,42 +1,67 @@
 import NavBar from "../components/NavBar.tsx";
 import Footer from "../components/Footer.tsx";
+import {useState} from "react";
 
 export default function Signup() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:8080/api/players/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                }),
+            });
+
+            if(response.ok) {
+                console.log("Account created successfully.");
+                return;
+            }
+            const errorData = await response.json();
+            console.log(errorData.message);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <>
             <NavBar />
 
             <main className="pt-16">
-                <div className="flex min-h-screen items-center justify-center px-6">
-                    <div
-                        className="w-full max-w-md p-10 space-y-6 rounded-lg shadow-lg bg-gray-700/10 border border-white/30">
+                <div className="flex min-h-[80svh] items-center justify-center">
+                    <div className="w-full max-w-md p-10 space-y-8 rounded-lg shadow-lg bg-gray-700/10 border border-white/30">
                         <h2 className="text-3xl font-bold text-center text-white">Create Account</h2>
-                        <form className="space-y-4">
+                        <form className="space-y-6" onSubmit={handleSubmit}>
                             {/* Username */}
                             <div>
-                                <label htmlFor="username" className="block text-sm font-medium text-gray-300">
-                                    Username
-                                </label>
                                 <input
                                     id="username"
-                                    name="username"
+                                    placeholder="Username"
                                     type="text"
                                     required
-                                    className="w-full mt-1 px-3 py-2 border border-gray-600 rounded-lg bg-gray-700 text-white focus:ring-blue-500 focus:border-blue-500"
+                                    onChange={event => setUsername(event.target.value)}
+                                    className="w-full mt-1 px-3 py-2 border border-white/30 rounded-lg bg-gray-700 text-white focus:ring-blue-500 focus:border-blue-500"
                                 />
                             </div>
 
                             {/* Password */}
                             <div>
-                                <label htmlFor="password" className="block text-sm font-medium text-gray-300">
-                                    Password
-                                </label>
                                 <input
                                     id="password"
-                                    name="password"
+                                    placeholder="Password"
                                     type="password"
                                     required
-                                    className="w-full mt-1 px-3 py-2 border border-gray-600 rounded-lg bg-gray-700 text-white focus:ring-blue-500 focus:border-blue-500"
+                                    onChange={event => setPassword(event.target.value)}
+                                    className="w-full mt-1 px-3 py-2 border border-white/30 rounded-lg bg-gray-700 text-white focus:ring-blue-500 focus:border-blue-500"
                                 />
                             </div>
 
