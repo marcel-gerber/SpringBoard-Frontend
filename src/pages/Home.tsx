b118d4aa7774c4b59d782e7e94623de2c096a764
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-06 00:16:42 +0100

    Added login href

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,7 +21,7 @@ export default function Home() {
                                     </p>
                                     <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
                                         <div className="rounded-md shadow">
-                                            <a href="/"
+                                            <a href="/login"
                                                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-sky-900 hover:bg-sky-700 md:py-4 md:text-lg md:px-10">
                                                 Login
                                             </a>
