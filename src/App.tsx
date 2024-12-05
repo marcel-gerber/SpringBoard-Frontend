96cd727d723db56557bb8b267888f9ad9b2d5530
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-06 00:16:50 +0100

    Added login route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./styles/App.css";
 import Home from "./pages/Home.tsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Signup from "./pages/./Signup.tsx";
+import Login from "./pages/Login.tsx";
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
     );
