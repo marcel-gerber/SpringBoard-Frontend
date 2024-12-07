23481fba05d691f2adec62625d3dcfb192d9e25f
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-07 22:11:13 +0100

    Added Games route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home.tsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Signup from "./pages/./Signup.tsx";
 import Login from "./pages/Login.tsx";
+import Games from "./pages/Games.tsx";
 
 export default function App() {
     return (
@@ -11,6 +12,7 @@ export default function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/games" element={<Games />} />
             </Routes>
         </BrowserRouter>
     );
