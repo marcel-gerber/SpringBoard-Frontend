fb904f6ad103328e36bc1dfdc01b07359df19109
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-09 23:08:04 +0100

    Implemented GameCard component

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,6 +1,7 @@
 import NavBar from "../components/NavBar.tsx";
 import Footer from "../components/Footer.tsx";
 import {useEffect, useState} from "react";
+import GameCard, {GameCardProps} from "../components/GameCard.tsx";
 
 export default function Games() {
     const [games, setGames] = useState([]);
@@ -9,9 +10,7 @@ export default function Games() {
     useEffect(() => {
         const fetchGames = async () => {
             try {
-                const response = await fetch("http://localhost:8080/api/games", {
-                    credentials: "include", // Send cookies (including httpOnly cookie)
-                });
+                const response = await fetch("http://localhost:8080/api/games");
 
                 if(response.ok) {
                     const data = await response.json();
@@ -34,11 +33,11 @@ export default function Games() {
                     {error ? (
                         <p className="text-red-500">{error}</p>
                     ) : (
-                        <ul>
+                        <div className="grid grid-cols-1 gap-4">
                             {games.map((game) => (
-                                <li key={game.id}>{game.fen}</li>
+                                <GameCard {...game as GameCardProps} />
                             ))}
-                        </ul>
+                        </div>
                     )}
                 </div>
             </main>
