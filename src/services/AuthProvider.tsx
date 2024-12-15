bc35a10d70620c7e6d720ab094a952889b57000b
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-15 23:46:01 +0100

    Added AuthProvider

---
+++ src/services/AuthProvider.tsx
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext({ isLoggedIn: false, login: () => {}, logout: () => {} });
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // Check for an active session
+    useEffect(() => {
+        const checkSession = async () => {
+            const response = await fetch("http://localhost:8080/api/players/check-session", { credentials: "include" });
+            setIsLoggedIn(response.ok);
+        };
+
+        checkSession();
+    }, []);
+
+    const login = () => setIsLoggedIn(true);
+    const logout = async () => {
+        await fetch("http://localhost:8080/api/players/logout", {
+            method: "POST",
+            credentials: "include",
+        });
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
