03eefd92fe9d67cf41197b48dbc8103258e1c6db
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-17 00:28:09 +0100

    Added error alert when user is not logged in

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -3,16 +3,20 @@ import Footer from "../components/Footer.tsx";
 import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import GameCard, {GameCardProps} from "../components/GameCard.tsx";
-import {Modal} from "flowbite-react";
+import {Alert, Modal} from "flowbite-react";
 import WhitePawn from "/pieces/wP.svg";
 import BlackPawn from "/pieces/bP.svg";
 import {Color} from "../chesslogic/Types.ts";
+import {useAuth} from "../services/AuthProvider.tsx";
+import {HiInformationCircle} from "react-icons/hi";
 
 export default function Games() {
     const [games, setGames] = useState([]);
     const [error, setError] = useState("");
     const [openModal, setOpenModal] = useState(false);
+    const [showError, setShowError] = useState(false);
 
+    const {isLoggedIn} = useAuth();
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -32,7 +36,15 @@ export default function Games() {
         fetchGames();
     }, []); // Empty dependency array to run only once
 
-    async function handleClick(color: Color) {
+    function handleClickOnCreateGame() {
+        if(!isLoggedIn) {
+            setShowError(true);
+            return;
+        }
+        setOpenModal(true);
+    }
+
+    async function handleClickOnColor(color: Color) {
         const response = await fetch("http://localhost:8080/api/games", {
             method: "POST",
             headers: {
@@ -54,15 +66,29 @@ export default function Games() {
             <NavBar/>
             <main className="pt-16">
                 <div className="p-4">
+                    {showError && (
+                        <div className="justify-center max-w-sm mx-auto mb-4">
+                            <Alert
+                                color="failure"
+                                onDismiss={() => setShowError(false)}
+                                icon={HiInformationCircle}
+                            >
+                                <span className="font-medium">You are not logged in!</span>
+                            </Alert>
+                        </div>
+                    )}
+
                     <h1 className="text-3xl font-extrabold mb-6 text-white relative
                     after:content-[''] after:block after:mt-2 after:w-full after:h-[2px] after:bg-white/70
                     after:max-w-[90%] after:mx-auto">
                         Games
                     </h1>
                     <div className="mb-6">
-                        <button onClick={() => setOpenModal(true)} className="px-8 py-2 text-white font-bold text-lg rounded-full shadow-lg
-                        transition-transform transform bg-transparent border-2 border-green-600 hover:scale-105
-                        hover:shadow-green-500/50 hover:shadow-2xl focus:outline-none">
+                        <button
+                            onClick={() => handleClickOnCreateGame()}
+                            className="px-8 py-2 text-white font-bold text-lg rounded-full shadow-lg
+                            transition-transform transform bg-transparent border-2 border-green-600 hover:scale-105
+                            hover:shadow-green-500/50 hover:shadow-2xl focus:outline-none">
                             Create a new game
                         </button>
                     </div>
@@ -92,7 +118,7 @@ export default function Games() {
                     <ul className="my-4 space-y-3">
                         <li>
                             <button
-                                onClick={() => handleClick(Color.WHITE)}
+                                onClick={() => handleClickOnColor(Color.WHITE)}
                                 className="flex items-center p-3 w-full text-base font-bold rounded-lg
                                 hover:shadow bg-gray-700/20 hover:bg-gray-700 text-white border border-white/30">
                                 <img src={WhitePawn} alt="Logo" className="w-8 h-8"/>
@@ -101,7 +127,7 @@ export default function Games() {
                         </li>
                         <li>
                             <button
-                                onClick={() => handleClick(Color.BLACK)}
+                                onClick={() => handleClickOnColor(Color.BLACK)}
                                 className="flex items-center p-3 w-full text-base font-bold rounded-lg
                                 hover:shadow bg-gray-700/20 hover:bg-gray-700 text-white border border-white/30">
                                 <img src={BlackPawn} alt="Logo" className="w-8 h-8"/>
