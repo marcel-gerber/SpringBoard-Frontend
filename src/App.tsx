83bbfb232f0c6f87347bda59bde13175ed3e1ad0
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-17 00:01:47 +0100

    Added AuthProvider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Signup from "./pages/./Signup.tsx";
 import Login from "./pages/Login.tsx";
 import Games from "./pages/Games.tsx";
+import {AuthProvider} from "./services/AuthProvider.tsx";
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/games" element={<Games />} />
-            </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/games" element={<Games />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     );
 }
