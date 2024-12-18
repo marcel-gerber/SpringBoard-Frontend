83fcd6ac13bf40bc68563843624912ba115cad28
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-18 23:43:52 +0100

    Added isLoggedIn check

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -4,10 +4,12 @@ import {useParams} from "react-router-dom";
 import Footer from "../components/Footer.tsx";
 import {useEffect, useState} from "react";
 import {GameCardProps} from "../components/GameCard.tsx";
+import {useAuth} from "../services/AuthProvider.tsx";
 
 export default function Game() {
     const [game, setGame] = useState<GameCardProps>({} as GameCardProps);
     const [error, setError] = useState("");
+    const {isLoggedIn} = useAuth();
     const {gameId} = useParams();
 
     useEffect(() => {
@@ -36,7 +38,9 @@ export default function Game() {
             <NavBar />
             <main className="pt-16">
                 <div className="flex justify-center p-4 mt-4">
-                    {game.fen && <Chessboard fen={game.fen} />}
+                    {isLoggedIn ? (game.fen && <Chessboard fen={game.fen} />)
+                        : (game.fen && <Chessboard fen={game.fen} readOnly={true} />)
+                    }
                 </div>
             </main>
             <Footer />
