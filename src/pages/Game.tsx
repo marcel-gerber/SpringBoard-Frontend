bf33ea863be664816f3e685c65c4dc004958baeb
Marcel Gerber <marcelgerber9@gmail.com>
2024-12-21 00:13:05 +0100

    Added game information card

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -5,6 +5,8 @@ import Footer from "../components/Footer.tsx";
 import {useEffect, useState} from "react";
 import {GameCardProps} from "../components/GameCard.tsx";
 import {useAuth} from "../services/AuthProvider.tsx";
+import WhitePawn from "/pieces/wP.svg";
+import BlackPawn from "/pieces/bP.svg";
 
 export default function Game() {
     const [game, setGame] = useState<GameCardProps>({} as GameCardProps);
@@ -38,10 +40,28 @@ export default function Game() {
             <NavBar />
             <main className="pt-16">
                 <div className="flex justify-center p-4 mt-4">
-                    {game.fen && <Chessboard fen={game.fen} readOnly={!isLoggedIn} />}
+                    {game.fen && <Chessboard fen={game.fen} readOnly={!isLoggedIn}/>}
+                </div>
+                <div className="absolute right-4 top-1/2 transform -translate-y-1/2 border border-white/20 bg-gray-700/10
+                rounded-lg p-4 w-64 text-white">
+                    <div className="mb-4">
+                        <div className="text-center flex items-center justify-center space-x-2 mr-10">
+                            <img src={BlackPawn} alt="Black Pawn" className="w-8 h-8"/>
+                            <h2 className="text-xl font-bold">Black</h2>
+                        </div>
+                        {game.playerBlack && <p className="text-center">{game.playerBlack.username}</p>}
+                    </div>
+
+                    <div className="">
+                        <div className="text-center flex items-center justify-center space-x-2 mr-10">
+                            <img src={WhitePawn} alt="White Pawn" className="w-8 h-8"/>
+                            <h2 className="text-xl font-bold">White</h2>
+                        </div>
+                        {game.playerWhite && <p className="text-center">{game.playerWhite.username}</p>}
+                    </div>
                 </div>
             </main>
-            <Footer />
+            <Footer/>
         </>
     );
 }
