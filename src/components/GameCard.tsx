24a498d2eb2b975398ad319b9152ba4544ef080e
Marcel Gerber <marcelgerber9@gmail.com>
2025-02-01 23:35:36 +0100

    Made GameCard responsive for mobile

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -19,18 +19,17 @@ export default function GameCard(props: GameCardProps & HandleJoinGameProps) {
     const waitingPlayerId: string = props.playerWhite ? props.playerWhite.id : props.playerBlack.id;
 
     return (
-        <div className="flex bg-white/10 backdrop-blur-md border border-white/20 rounded-lg p-4 shadow-md w-full max-w-4xl mx-auto">
-            <div className="w-1/2 flex justify-center items-center">
+        <div className="flex flex-col sm:flex-row bg-white/10 backdrop-blur-md border border-white/20 rounded-lg p-4 shadow-md w-full max-w-4xl mx-auto">
+            <div className="flex justify-center items-center sm:w-1/2 sm:mt-0">
                 <Chessboard fen={props.fen} readOnly={true} />
             </div>
 
-            <div className="w-1/2 flex flex-col justify-center space-y-4 pl-6">
+            <div className="flex flex-col sm:w-1/2 space-y-4 mt-4 sm:mt-0 sm:pl-6">
                 <Link to={`/games/${props.id}`}
                       className="text-white font-semibold hover:text-gray-300 transition">
                     <h2 className="text-lg font-bold text-white">Game ID: {props.id}</h2>
                 </Link>
-                <p className="text-sm text-gray-300 mt-2">State: {props.state}</p>
-                <div className="mt-4">
+                <div>
                     <h3 className="text-white font-semibold">Players</h3>
                     {props.playerWhite &&
                         <p className="text-gray-300">
@@ -43,12 +42,13 @@ export default function GameCard(props: GameCardProps & HandleJoinGameProps) {
                         </p>
                     }
                 </div>
-                <div className="mt-4">
+                <p className="text-sm text-gray-300">State: {props.state}</p>
+                <div>
                     <h3 className="text-white font-semibold">Moves</h3>
                     <p className="text-gray-300">{props.moves.join(", ") || "No moves yet"}</p>
                 </div>
                 {isWaitingForPlayer && (
-                    <div className="mt-4">
+                    <div>
                         <button
                             onClick={() => props.handleJoinGame(props.id, waitingPlayerId)}
                             className="px-8 py-2 text-white font-bold text-lg rounded-full shadow-lg
